caeea911d64d669bf8b09d4a8b5c0cd75ca80ecc
sambitkumarjethy <sambitkumarjethy@gmail.com>
2022-05-21 22:34:53 +0530

    use of class base component and conditional statement
    use of class base component and conditional statement

--- React/fundamentals/udemy/seasons/src/index.js
+++ React/fundamentals/udemy/seasons/src/index.js
@@ -1,11 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div> HII there</div>;
-};
+import ReactDOM, { render } from "react-dom";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: null };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        //!! WE CALLED setState
+        this.setState({ lat: position?.coords?.latitude });
+        //
+      },
+      (err) => {
+        this.setState({ errorMessage: err?.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error:{this?.state?.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude:{this?.state?.lat}</div>;
+    }
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));

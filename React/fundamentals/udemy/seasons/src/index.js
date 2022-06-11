5bb831747d498fe1fbafeeefe50cec652ec3d027
sambitkumarjethy <sambitkumarjethy@gmail.com>
2022-06-11 22:50:53 +0530

    Avoiding conditionals on Render

--- React/fundamentals/udemy/seasons/src/index.js
+++ React/fundamentals/udemy/seasons/src/index.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   componentDidUpdate() {
     console.log("componentDidUpdate");
   }
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error:{this?.state?.errorMessage}</div>;
     }
@@ -24,6 +24,9 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 

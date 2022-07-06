aa5746d537f71ef6c70bea369f5262e7e14d83b8
sambitkumarjethy <sambitkumarjethy@gmail.com>
2022-07-06 20:46:14 +0530

    08_making_api_request_react
    making_api_request_react -- using axios class with  Api of unsplash

--- React/fundamentals/udemy/pics/src/components/App.js
+++ React/fundamentals/udemy/pics/src/components/App.js
@@ -1,13 +1,41 @@
 import React from "react";
+import unsplash from "../api/unspalsh";
 import SearchBar from "./SearchBar";
+
 class APP extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+  // async onSearchSubmit(term) {
+  //   const response = await axios.get("https://api.unsplash.com/search/photos", {
+  //     params: {
+  //       query: term,
+  //     },
+  //     headers: {
+  //       Authorization: "Client-ID mFEhpyG5U73dBaiWYFN544oeOdu0Iw0Hwta1YmUVt50",
+  //     },
+  //   });
+  //   console.log(this);
+  //   console.log(response.data.results);
+  //   this.setState({ images: response.data.results });
+  // }
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get(
+      "https://api.unsplash.com/search/photos",
+      {
+        params: {
+          query: term,
+        },
+      }
+    );
+    console.log(this);
+    console.log(response.data.results);
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div class="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} Images
       </div>
     );
   }
